77fe61a6f4c1392a6e43a2b17b9398e60711123f
rayto <rayto@R-MacBook.local>
2024-06-23 20:07:31 +0900

    アプリ投稿と編集機能を改修

--- app/models/web_app.rb
+++ app/models/web_app.rb
@@ -2,7 +2,7 @@ class WebApp < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { maximum: 255 }
   validates :description, presence: true, length: { maximum: 65_535 }
-  validates :url, length: { maximum: 255 }
+  validates :url, presence: true, length: { maximum: 255 }
   validates :github_url, length: { maximum: 255 }
 
   mount_uploader :image, ImageUploader
